f9b0a834c9ee5888378e42551fdcac484fbff548
Nick <42703997+nicolasmaclean@users.noreply.github.com>
2020-03-26 15:58:09 -0500

    init

---
+++ Player Controller/Assets/Scipts/CharacterAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = 1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+    PlayerController playerController;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
