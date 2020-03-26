dcce8fe2769cc90d34734f138d13a8794a5851e6
Nick <42703997+nicolasmaclean@users.noreply.github.com>
2020-03-26 17:55:10 -0500

    added a little ui and shopping list

---
+++ Player Controller/Assets/Scipts/ShoppingListItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ShoppingListItem
+{
+    public string ItemName;
+    public int ItemAmount;
+    public int currentAmount;
+    public int ScoreReward;
+}
---
+++ Player Controller/Assets/Scipts/ShoppingListManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoppingListManager : MonoBehaviour
+{
+    public ShoppingListItem[] shoppingListItems;
+    public GameObject itemPrefab;
+    public VerticalLayoutGroup verticalLayoutGroup;
+    public GameObject[] items;
+
+    void Start()
+    {
+        items = new GameObject[shoppingListItems.GetLength(0)];
+        for(int i = 0; i < items.GetLength(0); i++) {
+            ShoppingListItem x = shoppingListItems[i];
+            GameObject item = Instantiate(itemPrefab, Vector3.zero, new Quaternion(0,0,0,0));
+            item.transform.SetParent(verticalLayoutGroup.transform);
+            item.GetComponent<Toggle>().isOn = false;
+            item.GetComponentInChildren<Text>().text = x.ItemName + "(" + x.currentAmount + "/" + x.ItemAmount + ")";
+            items[i] = item;
+        }
+    }
+
+    void updateItems()
+    {
+        for(int i = 0; i < items.GetLength(0); i++) {
+            ShoppingListItem x = shoppingListItems[i];
+            GameObject item = items[i];
+            item.transform.SetParent(verticalLayoutGroup.transform);
+            item.GetComponent<Toggle>().isOn = false;
+            item.GetComponentInChildren<Text>().text = x.ItemName + "(" + x.currentAmount + "/" + x.ItemAmount + ")";
+        }
+    }
+}
